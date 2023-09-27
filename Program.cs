f0ca3739aa577e4050bfbb47ad4852a76fe68012
KacperOtreba <kacperotreba1@gmail.com>
2023-09-27 08:24:50 +0200

    add

---
+++ Program.cs
@@ -0,0 +1,37 @@
+int i1 = 10; 
+int32 i2 = 10;
+System.Int32 i3 = 10;
+
+/*
+ Typy danych
+ int 16, 32, 64
+ byte 0-255
+ sbyte -128 do 127
+ short -32768 do 32767
+ int -2 147 483 648 do 2 147 483 647
+ 
+*/
+
+int i; //deklaracja zmiennej
+int i1 = 10; // deklaracje z jednoczesna inicjalizacja zmiennej i
+
+System.Console.WriteLine("Zmienna i wynosi: " + i1);
+
+System.Int32 i2 = 100;
+
+Console.WriteLine("Zmienna i1 wynosi:  {0}, zmienna i2 wynosi:  {1}", i1, i2);
+Console.WriteLine($"Zmienna i1 wynosi: {i1}, zmienna i2 wynosi: {i2}");
+
+byte b = 10; 
+sbyte sb = -10;
+short s = -20000;
+int incior = 2200000;
+
+// zapis systemów liczbowych
+// binarny
+System.Console.WriteLine(0b1011);
+
+// oktalny
+System.Console.WriteLine(011);
+int io = Convert.ToInt32("12", 8);
+System.Console.WriteLine(io); // 10 => 2x8^0 + 1x8^1 = 2 + 8 = 10(10)
