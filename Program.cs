23f3dfc0bbd12184771bde352df1afe0bed8bf7b
KacperOtreba <kacperotreba1@gmail.com>
2023-10-09 17:25:53 +0200

    fix

--- Program.cs
+++ Program.cs
@@ -1,59 +1,60 @@
-int i1 = 10; 
-int32 i2 = 10;
-System.Int32 i3 = 10;
+// int i1 = 10; 
+// Int32 i2 = 10;
+// System.Int32 i3 = 10;
 
-/*
- Typy danych
- int 16, 32, 64
- byte 0-255
- sbyte -128 do 127
- short -32768 do 32767
- int -2 147 483 648 do 2 147 483 647
- 
-*/
+//
+//  Typy danych
+//  int 16, 32, 64
+//  byte 0-255
+//  sbyte -128 do 127
+//  short -32768 do 32767
+//  int -2 147 483 648 do 2 147 483 647
 
-int i; //deklaracja zmiennej
-int i1 = 10; // deklaracje z jednoczesna inicjalizacja zmiennej i
+//
 
-System.Console.WriteLine("Zmienna i wynosi: " + i1);
+// int i; //deklaracja zmiennej
+// // int i1 = 10; // deklaracje z jednoczesna inicjalizacja zmiennej i
 
-System.Int32 i2 = 100;
+// System.Console.WriteLine("Zmienna i wynosi: " + i1);
 
-Console.WriteLine("Zmienna i1 wynosi:  {0}, zmienna i2 wynosi:  {1}", i1, i2);
-Console.WriteLine($"Zmienna i1 wynosi: {i1}, zmienna i2 wynosi: {i2}");
+// // System.Int32 i2 = 100;
 
-byte b = 10; 
-sbyte sb = -10;
-short s = -20000;
-int incior = 2200000;
+// Console.WriteLine("Zmienna i1 wynosi:  {0}, zmienna i2 wynosi:  {1}", i1, i2);
+// Console.WriteLine($"Zmienna i1 wynosi: {i1}, zmienna i2 wynosi: {i2}");
 
-// zapis systemów liczbowych
-// binarny
-System.Console.WriteLine(0b1011);
+// byte b = 10; 
+// sbyte sb = -10;
+// short s = -20000;
+// int incior = 2200000;
 
-// oktalny
-System.Console.WriteLine(011);
-int io = Convert.ToInt32("12", 8);
-System.Console.WriteLine(io); // 10 => 2x8^0 + 1x8^1 = 2 + 8 = 10(10)
+// // zapis systemów liczbowych
+// // binarny
+// System.Console.WriteLine(0b1011);
 
-//heksadecymalny
-System.Console.WriteLine(0xA1);
+// // oktalny
+// System.Console.WriteLine(011);
+// int io = Convert.ToInt32("12", 8);
+// System.Console.WriteLine(io); // 10 => 2x8^0 + 1x8^1 = 2 + 8 = 10(10)
 
-//zakres typów danych
-System.Console.WriteLine(byte.MinValue); //0
-System.Console.WriteLine(byte.MaxValue); //255
+// //heksadecymalny
+// System.Console.WriteLine(0xA1);
 
-System.Console.WriteLine(sbyte.MinValue); //-128
-System.Console.WriteLine(sbyte.MaxValue); //127
+// //zakres typów danych
+// System.Console.WriteLine(byte.MinValue); //0
+// System.Console.WriteLine(byte.MaxValue); //255
 
-System.Console.WriteLine(int.MinValue); // -
+// System.Console.WriteLine(sbyte.MinValue); //-128
+// System.Console.WriteLine(sbyte.MaxValue); //127
 
-System.Console.WriteLine(long.MinValue);
+// System.Console.WriteLine(int.MinValue); // -
 
-System.Console.WriteLine(Int128.MaxValue); // brak typu w .net 8. Istniał w 7
+// System.Console.WriteLine(long.MinValue);
 
-Console.OutputEncoding = System.Text.Encoding.Unicode;
-System.Console.WriteLine("♪");
-Console.OutputEncoding = System.Text.Encoding.Default;
+// System.Console.WriteLine(Int128.MaxValue); // brak typu w .net 8. Istniał w 7
 
-System.Console.WriteLine();
+// Console.OutputEncoding = System.Text.Encoding.Unicode;
+// System.Console.WriteLine("♪");
+// Console.OutputEncoding = System.Text.Encoding.Default;
+
+// System.Console.WriteLine();
+
