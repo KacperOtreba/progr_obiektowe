21ec23d86138ca5d0fcc0f468a7a7a52e41b975c
KacperOtreba <kacperotreba1@gmail.com>
2023-10-30 17:59:50 +0100

    nowe lekcje

---
+++ kartkowka24.10/Program.cs
@@ -0,0 +1,33 @@
+﻿// Gr2
+// napisz program który oblicza pole prostokąta
+using System;
+try{
+System.Console.WriteLine("Podaj dwa boki");
+int x = 0;
+int y = 0;
+while (x <= 0){
+    System.Console.Write("Podaj długość pierwszego boku w cm: ");
+    x = int.Parse(Console.ReadLine());
+    if (x <= 0){
+        System.Console.WriteLine("Bok nie może mieć długości ujemnej lub zerowej\n");
+    }
+}
+while (y <= 0){
+    System.Console.Write("Podaj długość drugiego boku w cm: ");
+    y = int.Parse(Console.ReadLine());
+    if (y <= 0){
+        System.Console.WriteLine("Bok nie może mieć długości ujemnej lub zerowej\n");
+    }
+}
+double result = x * y;
+System.Console.WriteLine($"Pole tego prostokąta to: {result}cm2");
+}
+catch(FormatException){
+    System.Console.WriteLine("Podane dane nie są prawidłowe");
+}
+catch(OverflowException){
+    System.Console.WriteLine($"Podana liczba jest zbyt duża lub zbyt mała, podaj liczbe w wielkościach <{int.MinValue}; {int.MaxValue}>");
+}
+catch (Exception ex){
+    System.Console.WriteLine($"Błąd: {ex.Message}");
+}
---
+++ lekcja_24.10.2023/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+void ErrorColorChange(string comment){
+    Console.ForegroundColor = ConsoleColor.Red;
+    System.Console.WriteLine($"\nBłąd :{comment}");
+    Console.ResetColor();
+}   
+
+try{
+System.Console.WriteLine("Podaj dwie liczby\n");
+System.Console.Write("Podaj a: ");
+int x = int.Parse(Console.ReadLine());
+System.Console.Write("Podaj y: ");
+int y = int.Parse(Console.ReadLine());
+// if (y == 0){
+//     throw new Exception("Nie można dzielić przez zero");
+// }
+int result = x/y;
+System.Console.Write($"Wynik dzielenia {x}/{y} = ");
+Console.ForegroundColor = ConsoleColor.Green;
+System.Console.WriteLine($"{result:F3}");
+Console.ResetColor();
+}
+catch (DivideByZeroException){
+    // System.Console.WriteLine("Nie można dzielić przez zero");
+    ErrorColorChange("Nie można dzielić przez zero");
+}
+catch (FormatException){
+    // System.Console.WriteLine("Błędny format danych, podaj odpowiednią liczbę");
+    ErrorColorChange("Błędny format danych, podaj odpowiednią liczbę");
+}
+catch (OverflowException){
+    // System.Console.WriteLine($"Podana liczba jest błędna, podaj dane z zakresu <{int.MinValue}; {int.MaxValue}>");
+    ErrorColorChange($"Podana liczba jest błędna, podaj dane z zakresu <{int.MinValue}; {int.MaxValue}>");
+}
+catch (Exception ex){
+    // System.Console.WriteLine($"Błąd: {ex.Message}");
+    ErrorColorChange(ex.Message);
+}
