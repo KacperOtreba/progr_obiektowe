2a1b4c91b193e735aaff738e4cbe90154c6744b9
KacperOtreba <kacperotreba1@gmail.com>
2023-11-14 10:56:41 +0100

    nowa lekcja

---
+++ lekcja_14.11.2023/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace lekcja_14._11._2023;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        // funkcja statyczna, która pozwala wprowadzić rozmiar tablicy i nazwę CreateArray()
+        // funkcja statyczna, która ustawia elementy tablicy SetArray()
+        // funkcja statyczna, która wyświetla zawartość tablicy DisplayArray()
+    }
+    public static int[] CreateArray(string name){
+        System.Console.Write($"Podaj rozmiar tablicy {name}: ");
+        int size = int.Parse(Console.ReadLine());
+        int[] array = new int[size];
+        return array;
+        
+    }
+    
+}
